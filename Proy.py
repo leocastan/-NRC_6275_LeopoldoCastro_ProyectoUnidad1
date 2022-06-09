65b41669f8aab6820b85650a523245871c8ca2be
lecastro3 <lecastro3@espe.edu.ec>
2022-06-09 00:01:51 -0500

    Avance 9

--- Proy.py
+++ Proy.py
@@ -1,6 +1,8 @@
 # Importar la biblioteca de flask y librerias necesarias
+from copyreg import pickle
 from tkinter import messagebox
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+from requests import request
 
 
 # Instanciar la aplicación
@@ -114,6 +116,48 @@ def quito():
 def sangolqui():
     '''Esta funcion muestra la página autoridades del sitio'''
     return render_template('sangolqui.html')
+
+
+# Arreglo para almacenar las tareas
+listaTareas = []
+
+# 1. Funcion controlador que muestra lista actual de tareas pendientes y un formulario para ingresar un nuevo elemento
+# Definicion de la ruta por defecto,
+@app.route('/ingreso')
+# Lamar a principal
+def ingreso():
+    return render_template('ingresoNotas.html', listaTareas=listaTareas)
+
+# 2. Funcion controlador para agregar lista a tarea de pendientes
+# Definicion de la ruta
+@app.route('/enviar', methods=['POST'])
+# Llamar a enviar
+def enviar():
+    # Funcion condicional para enviar los datos del formulario
+    if request.method == 'POST':
+
+        descripcion = request.form['descripcion']
+        correoElec = request.form['correoElec']
+        prioridad = request.form['prioridad']
+
+        # Funcion condicional para no registrar en caso de datos vacios
+        if descripcion == '' or correoElec == '' or prioridad == '':
+            #Mensaje de alerta de campos faltantes
+            messagebox.showwarning("¡Alerta!","Ingrese todos los campos")
+            return redirect(url_for('ingreso'))
+
+        else:
+            #Mensaje de autorizacion de registro
+            resultado = messagebox.askquestion("Registrar", "¿Está seguro que desea registrar los datos?")
+            #Funcion condicional de confirmacion de registro
+            if resultado == "yes":
+                listaTareas.append({'descripcion': descripcion, 'correoElec': correoElec, 'prioridad': prioridad })
+                return redirect(url_for('ingreso'))
+            else:
+                return redirect(url_for('ingreso'))
+
+
+
 
 
 # Metodo main del programa
